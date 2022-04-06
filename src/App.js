8b8c318b73482539aba70523334775f0e31f85a4
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2022-04-06 16:09:13 +0600

    firefox authentication intigrated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
 import './App.css';
+import app from './firebase.init';
+import {
+  getAuth,
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from 'firebase/auth';
+import { useState } from 'react';
 
 function App() {
+  const [user, setUser] = useState({});
+  const auth = getAuth(app);
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        setUser(result.user);
+        console.log(result.user);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const handleGithubSignIn = () => {
+    signInWithPopup(auth, githubProvider)
+      .then((result) => {
+        setUser(result.user);
+        console.log(result.user);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {user.uid ? (
+        <>
+          <div className='flex justify-center items-center h-[100vh]'>
+            <div className='w-1/2 h-96 bg-slate-100 rounded-3xlr'>
+              <button
+                className='block px-5 py-3 bg-red-500 text-white rounded-lg mt-3 mb-8 mx-auto'
+                onClick={handleSignOut}>
+                Sign Out
+              </button>
+              <div className='flex justify-center items-center'>
+                <div className='flex items-center bg-gray-800 text-white p-10 rounded-3xl'>
+                  <div className='w-24 h-24 mr-8'>
+                    <img
+                      src={user.photoURL}
+                      alt=''
+                      className='w-full rounded-full'
+                    />
+                  </div>
+                  <div>
+                    <h4 className='text-2xl font-bold mb-2'>
+                      {user.displayName}
+                    </h4>
+                    <p className='text-xl font-semibold'>{user.email}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : (
+        <div className='flex justify-center items-center h-[100vh]'>
+          <div className='w-1/2 h-96 bg-slate-100 rounded-3xl'>
+            <button
+              className='block px-5 py-3 bg-blue-400 rounded-lg my-2 mx-auto mt-10'
+              onClick={handleGoogleSignIn}>
+              Google Sign In
+            </button>
+            <button
+              className='block px-5 py-3 bg-green-400 rounded-lg my-2 mx-auto'
+              onClick={handleGithubSignIn}>
+              Github Sign In
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCeJpWTziQCM6YOsUJiT67Sr8q-nElevD4',
+  authDomain: 'fir-intro-46826.firebaseapp.com',
+  projectId: 'fir-intro-46826',
+  storageBucket: 'fir-intro-46826.appspot.com',
+  messagingSenderId: '237249273962',
+  appId: '1:237249273962:web:16418b5859543811a96a13',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
